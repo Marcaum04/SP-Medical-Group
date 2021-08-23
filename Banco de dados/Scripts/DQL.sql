316910204a42c4d30a8716c3a0ec6e09d93bb641
Marcos <mvinicius.oliveira04@gmail.com>
2021-08-23 11:26:42 -0300

    DQL Finalizado com procedures e pa

--- Banco de dados/Scripts/DQL.sql
+++ Banco de dados/Scripts/DQL.sql
@@ -1,11 +1,22 @@
 USE SPMED_MARCAUM;
 GO
 
-CREATE FUNCTION MedicosEspecialidade();
-RETURNS TABLE 
-AS 
-RETURN 
-SELECT 
-NomeUsuario
-Descricao
-IdMedico
+--Criou uma função para retornar a quantidade de médicos de uma determinada especialidade
+SELECT COUNT(IdMedico)
+FROM Medico
+WHERE IdEspecialidade = 17
+GO
+
+--Criou uma função para retornar a quantidade de médicos de uma determinada especialidade
+ALTER PROCEDURE P_Idade
+AS  
+SELECT  NomeUsuario, DATEDIFF(YEAR, (DataNascimento), GETDATE()) AS 'Idade'
+FROM Paciente
+INNER JOIN Usuario 
+ON Paciente.IdUsuario = Usuario.IdUsuario
+GO
+
+EXEC P_Idade;
+GO
+
+SELECT * FROM Paciente
